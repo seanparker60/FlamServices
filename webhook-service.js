dca7fdc355f62f7696ef55cbe6e9d57154c2e1c0
SParkerinfinitaslearning <sparker@infinitaslearning.com>
2026-06-27 16:34:21 +0200

    DD

--- webhook-service.js
+++ webhook-service.js
@@ -96,14 +96,22 @@ app.post('/slack-listener', async (req, res) => {
 
             // 🎯 STEP 3: Broadcast using your system's native ID room
            // io.to(INTERNAL_CONVERSATION_ID).emit('new_agent_comment', {
-           console.log(`📢 Map Success! Slack ${SLACK_CHANNEL_ID}`);
-           io.to(SLACK_CHANNEL_ID).emit('new_agent_comment', {
+           console.log(`📢 Map Success! before Slack ${SLACK_CHANNEL_ID}`);
+           /*io.to(SLACK_CHANNEL_ID).emit('new_agent_comment', {
                // conversation_id: INTERNAL_CONVERSATION_ID, // Mobile app recognizes this!
                conversation_id: SLACK_CHANNEL_ID,
                 message_text: incomingText,
                 source: 'Slack_Web',
                 created_at: new Date()
             });
+            */
+            io.to(SLACK_CHANNEL_ID).emit('new_agent_comment', {
+                SLACK_CHANNEL_ID,
+                message_text: incomingText,
+                source: 'Slack_Web',
+                created_at: new Date()
+            });
+            console.log(`📢 Map Success! after Slack ${SLACK_CHANNEL_ID}`);
 
         } catch (dbError) {
             console.error("🚨 Database lookup failed during Slack routing pipeline:", dbError);
