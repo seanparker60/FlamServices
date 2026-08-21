02403c89b3686b0c4d5301f9e20a42a8dd195de2
SParkerinfinitaslearning <sparker@infinitaslearning.com>
2026-08-21 14:41:39 +0200

    DB1

--- dashboard-service.js
+++ dashboard-service.js
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'Public'), { index: false }));
 
 // Configuration map for all your microservices
 const servicesConfig = {
